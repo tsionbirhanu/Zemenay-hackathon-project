0222aaa81740c5bf7e8355f8705c88771c9cf6d3
Tsion Birhanu <tsionbirhanu08@gmail.com>
2025-08-13 16:10:05 -0700

    Update UI styles

--- src/components/network-visualization.tsx
+++ src/components/network-visualization.tsx
@@ -1,14 +1,15 @@
+
 "use client";
 import Image from "next/image";
 
 export function NetworkVisualization() {
   return (
-    <div className="absolute top-0 right-0 w-[300px] h-[625px] pointer-events-none overflow-hidden">
+    <div className="absolute top-0 right-0 w-[300px] h-[625px] pointer-events-none">
       <Image
         src="/globe1.svg"
         alt="Globe"
-        fill
-        className="object-cover opacity-90 mix-blend-screen scale-[1.01]"
+        fill 
+        className="object-contain opacity-90 mix-blend-screen"
       />
     </div>
   );
